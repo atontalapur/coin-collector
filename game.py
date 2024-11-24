3107d98c2eab1abee82a0c4874ca14daf288379a
Valientkyton <akunn001@ucr.edu>
2024-11-24 02:15:20 -0800

    Added slight changes to make the files compatible with unit tests

--- game.py
+++ game.py
@@ -2,17 +2,21 @@ import arcade
 from level import Level
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, LEVEL_SETTINGS
 
+
 class Game(arcade.View):
     """Main application class."""
+
     def __init__(self, level):
         """Initialize the game window."""
         super().__init__()
         self.lvl = level
 
         self.box_x, self.box_y, self.box_width, self.box_height = SCREEN_WIDTH - 320, SCREEN_HEIGHT - 40, 285, 30
-        
+
         self.setup()
-    
+
+        self.window_closed=False
+
     def setup(self):
         # all level components
         self.level = Level(self.lvl)
@@ -33,13 +37,15 @@ class Game(arcade.View):
         arcade.start_render()
         self.level.draw()
         self.draw_time_box()
-    
+
     def draw_time_box(self):
         # Draw the box
-        arcade.draw_lrtb_rectangle_filled(self.box_x, self.box_x + self.box_width, self.box_y + self.box_height, self.box_y, arcade.color.LIGHT_GRAY)
+        arcade.draw_lrtb_rectangle_filled(self.box_x, self.box_x + self.box_width, self.box_y + self.box_height,
+                                          self.box_y, arcade.color.LIGHT_GRAY)
 
         # Draw the outline
-        arcade.draw_lrtb_rectangle_outline(self.box_x, self.box_x + self.box_width, self.box_y + self.box_height, self.box_y, arcade.color.BLACK, 2)
+        arcade.draw_lrtb_rectangle_outline(self.box_x, self.box_x + self.box_width, self.box_y + self.box_height,
+                                           self.box_y, arcade.color.BLACK, 2)
 
         # Draw the text inside the box
         text = f"Coins left : {len(self.level.environment.coin_list)}     Time: {int(self.time_elapsed) if self.time_elapsed < 1000 else 999}"
@@ -51,6 +57,7 @@ class Game(arcade.View):
 
         # Quit
         if key == arcade.key.Q:
+            self.window_closed = True
             arcade.exit()
         # Restart level environment
         elif key == arcade.key.R:
@@ -62,7 +69,7 @@ class Game(arcade.View):
     def on_key_release(self, key, modifiers):
         """Keys that are released."""
         self.movement_release(key)
-    
+
     def movement_press(self, key):
         """Checks if movement keys are pressed down. Supports arrow keys and WASD."""
         if key == arcade.key.UP or key == arcade.key.W:
@@ -84,8 +91,6 @@ class Game(arcade.View):
             self.level.environment.player.moving_left = False
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.level.environment.player.moving_right = False
-
-
 
 
 class PauseView(arcade.View):
@@ -130,8 +135,8 @@ class PauseView(arcade.View):
                          font_size=20,
                          anchor_x="center")
 
-    def on_key_press(self, key, _modifiers):
-        if key == arcade.key.ESCAPE:   # resume game
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.ESCAPE:  # resume game
             self.window.show_view(self.game_view)
 
             background_color = self.level_settings["BACKGROUND_COLOR"]
