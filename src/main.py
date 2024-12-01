b16fd701a872195158cb3646e5fef40827af535b
dpxa <Andrewmac2022@gmail.com>
2024-11-30 19:32:09 -0800

    finish pause func / add controller class / organize

--- src/main.py
+++ src/main.py
@@ -1,14 +1,4 @@
-import arcade
-from home import GameHome
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
-
-def main():
-    """Main function"""
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.set_location(0, 30)
-    game_view = GameHome()
-    window.show_view(game_view)
-    arcade.run()
-
+from controller import Controller
+    
 if __name__ == "__main__":
-    main()
+    controller = Controller()
