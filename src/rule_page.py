c054a10e17bbda503defdbff5b1eb26cf2c39164
atontalapur <advaith.mysuru@gmail.com>
2024-12-01 23:29:30 -0800

    Added rules to the game

--- src/rule_page.py
+++ src/rule_page.py
@@ -32,6 +32,59 @@ class Rule_Page(arcade.View):
             italic=True,
             font_name="Kenney High"
         )
+
+        self.rule1 = arcade.Text(
+            text="1) Collect all the coins in 60 seconds to win",
+            start_x=SCREEN_WIDTH // 2,
+            start_y=SCREEN_HEIGHT - 200,
+            color=arcade.color.YELLOW,
+            font_size=30,
+            anchor_x="center",
+            anchor_y="center",
+            bold=True,
+            italic=True,
+            font_name="Kenney High"
+        )
+
+        self.rule2 = arcade.Text(
+            text="2) Press escape to pause the game, play a different level, or restart the game",
+            start_x=SCREEN_WIDTH // 2,
+            start_y=SCREEN_HEIGHT - 260,
+            color=arcade.color.YELLOW,
+            font_size=30,
+            anchor_x="center",
+            anchor_y="center",
+            bold=True,
+            italic=True,
+            font_name="Kenney High"
+        )
+
+        self.rule3 = arcade.Text(
+            text="3) Use Up, Down, Left, and Right arrow keys or W,A,S,D keys to move the player",
+            start_x=SCREEN_WIDTH // 2,
+            start_y=SCREEN_HEIGHT - 320,
+            color=arcade.color.YELLOW,
+            font_size=30,
+            anchor_x="center",
+            anchor_y="center",
+            bold=True,
+            italic=True,
+            font_name="Kenney High"
+        )
+
+        self.rule4 = arcade.Text(
+            text="4) Have fun!",
+            start_x=SCREEN_WIDTH // 2,
+            start_y=SCREEN_HEIGHT - 380,
+            color=arcade.color.YELLOW,
+            font_size=30,
+            anchor_x="center",
+            anchor_y="center",
+            bold=True,
+            italic=True,
+            font_name="Kenney High"
+        )
+
         arcade.set_background_color(arcade.color.COOL_GREY)
 
         self.manager = arcade.gui.UIManager()
@@ -40,7 +93,7 @@ class Rule_Page(arcade.View):
         self.v_box = arcade.gui.UIBoxLayout(space_between=10, vertical=False)
         self.play_button = arcade.gui.UIFlatButton(text="Play", width=200)
 
-        self.v_box.add(self.play_button.with_space_around(top=200))
+        self.v_box.add(self.play_button.with_space_around(top=300))
         self.manager.add(
             arcade.gui.UIAnchorWidget(
                 align_x=0,
@@ -71,3 +124,7 @@ class Rule_Page(arcade.View):
         self.heading_text.draw()
         self.new_player.draw()
         self.manager.draw()
+        self.rule1.draw()
+        self.rule2.draw()
+        self.rule3.draw()
+        self.rule4.draw()
