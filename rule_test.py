51f65bc796abeeaf3e6f7c1b7929cd39c27a0e3b
atontalapur <advaith.mysuru@gmail.com>
2024-12-06 12:12:40 -0800

    Added rest of the rule test cases

--- rule_test.py
+++ rule_test.py
@@ -22,11 +22,11 @@ def test_rules() :
     window.show_view(view)
     assert view.rule1.text == "1) Collect all the coins in 60 seconds to win"
     assert view.rule2.text == "2) Press escape to pause the game, play a different level, or restart the game"
-    assert view.rule3.text == "3) Use arrow keys to move the player"
-    assert view.rule4.text== "4) Use space bar to jump"
+    assert view.rule3.text == "3) Use Up, Down, Left, and Right arrow keys or W,A,S,D keys to move the player"
+    assert view.rule4.text== "4) Have fun!"
 
 def test_game_launch() :
     window = arcade.Window(1200, 650, "Test Window")
     view = Rule_Page(1)
     window.show_view(view)
-    assert view.play_button.on_click == view.game_open()
+    assert view.play_button.on_click == view.game_open(None)
